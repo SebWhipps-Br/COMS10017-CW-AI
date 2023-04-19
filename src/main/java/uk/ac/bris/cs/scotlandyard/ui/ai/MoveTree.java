443b7abe24a4b8c99a0b255e05ab9a32d0a48f97
Alexander Wood <alexljwood24@hotmail.co.uk>
2023-04-19 20:51:52 +0100

    dijkstra & move tree

---
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MoveTree.java
@@ -0,0 +1,90 @@
+package uk.ac.bris.cs.scotlandyard.ui.ai;
+
+import uk.ac.bris.cs.scotlandyard.model.Board;
+import uk.ac.bris.cs.scotlandyard.model.Move;
+import uk.ac.bris.cs.scotlandyard.model.Piece;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MoveTree {
+    private final int source;
+
+    private final List<Node> children;
+
+    public MoveTree(int source, List<Node> children) {
+        this.source = source;
+        this.children = children;
+    }
+
+    public static MoveTree generate(Board.GameState board, int depth) {
+        Piece mrX = board.getPlayers().stream().filter(Piece::isMrX).findFirst().orElseThrow();
+        int mrXLocation = board.getAvailableMoves().stream().filter(move -> move.commencedBy().isMrX()).findFirst().orElseThrow().source(); // all the moves should start at the same position
+
+        MoveTree root = new MoveTree(mrXLocation, new ArrayList<>());
+        generate(root, board, mrX, mrXLocation, depth, Double.POSITIVE_INFINITY, Double.NEGATIVE_INFINITY);
+
+        return root;
+    }
+
+    /**
+     * Generates a subtree of possible moves, and adds them to the given tree.
+     * <p>
+     * Alpha =
+     */
+    public static void generate(MoveTree tree, Board.GameState board, Piece player, int source, int depth, Double alpha, Double beta) {
+        List<Move> moves = board.getAvailableMoves().stream().filter(move -> move.commencedBy().equals(player)).toList();
+        List<Node> children = moves.stream().map(move -> new Node(move, generate(board, player, source, move, depth), MyAi.dijkstra(board, move))).toList();
+        tree.children.addAll(children);
+    }
+
+    public static MoveTree generate(Board.GameState board, Piece player, int source, Move startingAt, int depth) {
+        if (depth <= 0) {
+            return new MoveTree(startingAt.source(), List.of());
+        }
+
+        Board.GameState newBoard = board.advance(startingAt);
+
+        List<Move> list = newBoard.getAvailableMoves().stream().toList();
+        List<Node> trees = list.stream().map(move ->
+                new Node(move,
+                        generate(newBoard, move.commencedBy(), move.source(), move, depth - 1),
+                        MyAi.dijkstra(board, move))).toList();
+
+        return new MoveTree(source, trees);
+    }
+
+    public int size() {
+        return 1 + children.stream().mapToInt(i -> i.child.size()).sum();
+    }
+
+
+    @Override
+    public String toString() {
+        return "MoveTree{" +
+                "source=" + source +
+                ", children=" + children +
+                '}';
+    }
+
+    static class Node {
+        private final Move move;
+        private final MoveTree child;
+
+        private final Double score;
+
+        Node(Move move, MoveTree child, Double score) {
+            this.move = move;
+            this.child = child;
+            this.score = score;
+        }
+
+        @Override
+        public String toString() {
+            return "Node{" +
+                    "move=" + move +
+                    ", child=" + child +
+                    '}';
+        }
+    }
+}
--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -1,13 +1,5 @@
 package uk.ac.bris.cs.scotlandyard.ui.ai;
 
-import java.sql.SQLOutput;
-import java.util.*;
-import java.util.concurrent.TimeUnit;
-
-
-import javax.annotation.Nonnull;
-
-
 import com.google.common.collect.ImmutableSet;
 import io.atlassian.fugue.Pair;
 import uk.ac.bris.cs.scotlandyard.model.Ai;
@@ -15,17 +7,107 @@ import uk.ac.bris.cs.scotlandyard.model.Board;
 import uk.ac.bris.cs.scotlandyard.model.Move;
 import uk.ac.bris.cs.scotlandyard.model.Piece;
 
-import static java.util.stream.Collectors.summingDouble;
+import javax.annotation.Nonnull;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 public class MyAi implements Ai {
 
-	@Nonnull @Override public String name() { return "Dijkstratron"; }
+    private static Integer moveDestination(Move move) {
+        Move.Visitor<Integer> destinationChecker = new Move.Visitor<Integer>() {
+            @Override
+            public Integer visit(Move.SingleMove move) {
+                return move.destination;
+            }
 
-	@Nonnull @Override public Move pickMove(
-			@Nonnull Board board,
-			Pair<Long, TimeUnit> timeoutPair) {
+            @Override
+            public Integer visit(Move.DoubleMove move) {
+                return move.destination2;
+            }
+        };
+        return move.accept(destinationChecker);
+    }
 
-		List<Move> moves = board.getAvailableMoves().asList();
+    private static Set<Integer> getDetectiveLocations(Board board) {
+        Set<Integer> detectiveLocations = new HashSet<>();
+        for (Piece piece : board.getPlayers()) {
+            if (piece.isDetective()) {
+                detectiveLocations.add(board.getDetectiveLocation((Piece.Detective) piece).get());
+            }
+        }
+        return detectiveLocations;
+    }
+
+    public static Double dijkstra(Board board, Move move) {
+        Set<Integer> detectiveLocations = getDetectiveLocations(board);
+
+        List<Integer> unvisited = new ArrayList<>(board.getSetup().graph.nodes());
+        System.out.println(unvisited.toString());
+
+        return 1.0;
+    }
+
+    /*
+    Calculates the shortest path / distance from a Mr X source position to a destination (probably a detective location)
+     */
+    public static double dijkstra(Board board, int source, int destination) {
+        Map<Integer, Integer> dist = new HashMap<>();
+        Map<Integer, Integer> prev = new HashMap<>();
+        record NodeInfo(int v, int dist) implements Comparable<NodeInfo> {
+            @Override
+            public int compareTo(NodeInfo o) {
+                return Integer.compare(this.dist, o.dist);
+            }
+        }
+
+        PriorityQueue<NodeInfo> priorityQueue = new PriorityQueue<>();
+
+        var graph = board.getSetup().graph;
+        for (Integer node : graph.nodes()) {
+            if (node != source) {
+                dist.put(node, Integer.MAX_VALUE);
+                prev.put(node, null);
+            }
+            priorityQueue.add(new NodeInfo(node, dist.get(node)));
+        }
+
+        while (!priorityQueue.isEmpty()) {
+            NodeInfo u = priorityQueue.poll(); // extract the minimum / best vertex
+            for (Integer v : graph.adjacentNodes(u.v)) {
+                int alt = dist.get(u.v) + graph.edgeValue(u.v, v).orElse(ImmutableSet.of()).size();
+                if (alt < dist.get(u.v)) {
+                    dist.put(v, alt);
+                    prev.put(v, u.v);
+                    priorityQueue.remove(u);
+                    u = new NodeInfo(u.v, alt);
+                    priorityQueue.add(u);
+                }
+            }
+        }
+
+        return 1.0;
+
+    }
+
+    private static Double dijkstraScore(Set<Double> distances) {
+        //Double sum = distances.stream().collect(summingDouble(x -> x));
+        Double shortest = Collections.min(distances);
+        return shortest;
+    }
+
+    @Nonnull
+    @Override
+    public String name() {
+        return "Dijkstratron";
+    }
+
+    @Nonnull
+    @Override
+    public Move pickMove(
+            @Nonnull Board board,
+            Pair<Long, TimeUnit> timeoutPair) {
+
+        List<Move> moves = board.getAvailableMoves().asList();
 //		System.out.println(moves);
 //		System.out.println(moves.get(0));
 //		System.out.println(moves.get(0).hashCode());
@@ -34,155 +116,43 @@ public class MyAi implements Ai {
 //		System.out.println(checkDoubleMove(moves.get(0)));
 //
 //		System.out.println(getDetectiveLocations(board));
-		//dijkstra for each possible move
-		Dictionary<Move, Double> moveScores = new Hashtable<>();
-		Double bestScore = 0.0; //higher is better
-		Move best = moves.get(0);
-		for (Move move : moves) {
-			Double d = dijkstra(board, move);
-			if (d > bestScore) {
-				bestScore = d;
-				best = move;
-			}
-			moveScores.put(move, d);
-
-		}
-
-		return best;
-		// returns a random move, replace with your own implementation
-		//return moves.get(new Random().nextInt(moves.size()));
-	}
-
-	private boolean checkDoubleMove(Move move){
-		Move.Visitor<Boolean> doubleMoveChecker = new Move.Visitor<Boolean>() {
-			@Override
-			public Boolean visit(Move.SingleMove move) {
-				return false;
-			}
-
-			@Override
-			public Boolean visit(Move.DoubleMove move) {
-				return true;
-			}
-		};
-		return move.accept(doubleMoveChecker);
-	}
-
-	private Integer moveDestination(Move move){
-		Move.Visitor<Integer> destinationChecker = new Move.Visitor<Integer>() {
-			@Override
-			public Integer visit(Move.SingleMove move) {
-				return move.destination;
-			}
-
-			@Override
-			public Integer visit(Move.DoubleMove move) {
-				return move.destination2;
-			}
-		};
-		return move.accept(destinationChecker);
-	}
-
-	private Set<Integer> getDetectiveLocations(Board board) {
-		Set<Integer> detectiveLocations = new HashSet<>();
-		for (Piece piece : board.getPlayers()) {
-			if (piece.isDetective()) {
-				detectiveLocations.add( board.getDetectiveLocation((Piece.Detective) piece).get() );
-			}
-		}
-		return detectiveLocations;
-	}
-
-	private Double dijkstra(Board board, Move move) {
-		Set<Integer> detectiveLocations = getDetectiveLocations(board);
-
-		List<Integer> unvisited = new ArrayList<>(board.getSetup().graph.nodes());
-		System.out.println(unvisited.toString());
-		System.out.println(board.getSetup().graph.toString());
-		System.out.println(board.getSetup().graph.adjacentNodes(move.source()));
-		Dictionary<Integer, Double> distances = new Hashtable<>();
-		for (Integer node : board.getSetup().graph.nodes()) {
-			distances.put(node, Double.POSITIVE_INFINITY);
-		}
-
-		Integer destination = moveDestination(move);
-		distances.put(destination, 0.0);
-		Set<Integer> visited = new HashSet<>();
-
-		LinkedHashSet<Integer> toVisitSet = new LinkedHashSet<>();
-		ArrayList<Integer> toVisitArray = new ArrayList<>();
-
-		boolean finished = false;
-		Integer current = destination;
-		toVisitSet.add(current);
-		toVisitArray.add(current);
-		List<Integer> adjacentUnvisited;
-		Double weight = 1.0;
-		int arrayIndex = 0;
-		while (!finished) {
-			adjacentUnvisited = board.getSetup().graph.adjacentNodes(current).stream().toList();
-			for (Integer node : visited) {
-				adjacentUnvisited.remove(node);
-			}
-			for (Integer node : adjacentUnvisited){
-				if (toVisitSet.add(node)) toVisitArray.add(node);
-
-				if (distances.get(node) > distances.get(current) + weight){
-					distances.put(node, distances.get(current) + weight);
-				}
-			}
-			visited.add(current);
-			if (toVisitArray.isEmpty()){
-				finished = true;
-			} else {
-				System.out.println("!   :" +toVisitArray.toString());
-				//toVisitArray.remove(0);
-				arrayIndex += 1;
-				current = toVisitArray.get(arrayIndex);
-			}
-
-		}
+        //dijkstra for each possible move
 
 
+        MoveTree tree = MoveTree.generate((Board.GameState) board, 3);
+//
+//        Dictionary<Move, Double> moveScores = new Hashtable<>();
+//        Double bestScore = 0.0; //higher is better
+//        Move best = moves.get(0);
+//        for (Move move : moves) {
+//            Double d = dijkstra(board, move);
+//            if (d > bestScore) {
+//                bestScore = d;
+//                best = move;
+//            }
+//            moveScores.put(move, d);
+//
+//        }
+//
+//        return best;
+        // returns a random move, replace with your own implementation
+        return moves.get(new Random().nextInt(moves.size()));
+    }
 
+    private boolean checkDoubleMove(Move move) {
+        Move.Visitor<Boolean> doubleMoveChecker = new Move.Visitor<Boolean>() {
+            @Override
+            public Boolean visit(Move.SingleMove move) {
+                return false;
+            }
 
-
-
-
-
-		/*
-		int i = 0;
-		while (unvisited.size() > i-1) {
-
-			Integer currentNode = unvisited.get(i);
-			System.out.println(currentNode.toString()+"\n");
-			Set<Integer> adjacentNodes = board.getSetup().graph.adjacentNodes(currentNode);
-			final int weight = 1;
-			for (Integer node : adjacentNodes) {
-				if (distances.get(node) > distances.get(currentNode) + weight){
-					distances.put(node, (distances.get(currentNode) + weight) );
-				}
-			}
-			i += 1;
-
-		}
-
-		 */
-
-		// now lookup how close the detectives are
-		Set<Double> detectiveDistances = new HashSet<>();
-		for (Integer location : detectiveLocations) {
-			detectiveDistances.add(distances.get(location));
-		}
-
-		return dijkstraScore(detectiveDistances);
-
-	}
-	private Double dijkstraScore(Set<Double> distances){
-		//Double sum = distances.stream().collect(summingDouble(x -> x));
-		Double shortest = Collections.min(distances);
-		return shortest;
-	}
+            @Override
+            public Boolean visit(Move.DoubleMove move) {
+                return true;
+            }
+        };
+        return move.accept(doubleMoveChecker);
+    }
 
 
 }
