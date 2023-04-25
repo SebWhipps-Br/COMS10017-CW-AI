dd0bf123904bd24f5f15efd2319c00573eed55a1
SebWhipps-Br <nk22674@bristol.ac.uk>
2023-04-25 11:06:47 +0100

    implemented timeout condition

--- src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
+++ src/main/java/uk/ac/bris/cs/scotlandyard/ui/ai/MyAi.java
@@ -8,8 +8,12 @@ import uk.ac.bris.cs.scotlandyard.model.Piece;
 
 import javax.annotation.Nonnull;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 public class MyAi implements Ai {
 
@@ -36,33 +40,44 @@ public class MyAi implements Ai {
     public Move pickMove(
             @Nonnull Board board,
             Pair<Long, TimeUnit> timeoutPair) {
-        //TODO pre evaluate the position
-        // if the position is poor consider and we have double moves -> doubles and reduce depth
-
+        try {
+            return CompletableFuture.supplyAsync(
+                    () -> pickGoodMove(board))
+                    .get(timeoutPair.left() - 1, timeoutPair.right());
+        } catch (ExecutionException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } catch (TimeoutException e) {
+            System.out.println("Move timout : picking random move");
+            return pickRandomMove(board);
+        }
+    }
+        private Move pickGoodMove(Board board){
         int mrXLocation = board.getAvailableMoves()
                 .stream()
                 .filter(m -> m.commencedBy().isMrX())
                 .findFirst().orElseThrow().source();
-        boolean doubleMoveAvailible = board.getAvailableMoves().stream()
+        boolean doubleMoveAvailable = board.getAvailableMoves().stream()
                 .anyMatch(MyAi::checkDoubleMove);
 
         double currentPositionScore = Dijkstra.dijkstraScore(MoveTree.getDetectiveDistances(board, mrXLocation));
 
-        boolean allowDoubleMove = false;
-        if (currentPositionScore < 2 && doubleMoveAvailible) {
-            allowDoubleMove = true;
-        }
-        //
+        boolean allowDoubleMove = currentPositionScore < 2 && doubleMoveAvailable;
+
         //dijkstra for each possible move
-        int depth = 4;
+        int depth = allowDoubleMove ? 4 : 8;
         MoveTree tree = MoveTree.generateRootTree((Board.GameState) board, depth, allowDoubleMove);
         System.out.println("1");
         MiniMax miniMax = new MiniMax();
         System.out.println("2");
          // all the moves should start at the same position
 
-        System.out.println(board.getAvailableMoves());
+        System.out.println("tree size" + tree.size());
         return miniMax.minimax(tree, depth, board, mrXLocation).move();
+    }
+
+    private Move pickRandomMove(Board board){
+        var moves = board.getAvailableMoves().asList();
+        return moves.get(new Random().nextInt(moves.size()));
     }
 
     public static boolean checkDoubleMove(Move move) {
